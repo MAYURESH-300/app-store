060acfe14b17c150c1ecc837b4352f0c6eff825a
MAYURESH-300 <mayureshfulore300@gmail.com>
2025-07-14 16:14:41 +0530

    Add files via upload

---
+++ base.js
@@ -0,0 +1,14 @@
+// Handle contact form (just for UI demonstration)
+// document.getElementById("contactForm").addEventListener("submit", function(e) {
+//   e.preventDefault();
+//   alert("Thank you for contacting us!");
+// });
+
+
+
+
+
+function toggleMenu() {
+  const nav = document.getElementById('navMenu');
+  nav.classList.toggle('nav-active');
+}
